329864ad64c1affb9b7d333ecab184e657f251dc
harshitvora <harshitvora@ccs.neu.edu>
2018-04-21 09:02:26 -0400

    Basic webapp example

---
+++ webapp.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"io/ioutil"
+	"encoding/xml"
+)
+
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News map[string]NewsMap
+}
+
+func main(){
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg", newsAggHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	var s SitemapIndex
+	var n News
+
+	news_map := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+
+		for idx, _ := range n.Titles{
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing news aggregator", News: news_map}
+	t, _ := template.ParseFiles("newsaggtemplate.html")
+	t.Execute(w, p)
+}
